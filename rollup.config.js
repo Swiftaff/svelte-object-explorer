d4531b26d40adef787fd97ca47c83a9917f00538
Swiftaff <swiftaff@gmail.com>
2021-07-18 14:04:34 +0800

    npm update

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json";
-import css from "rollup-plugin-css-only";
 import rolluppluginiconifysvg from "rollup-plugin-iconify-svg";
 import { terser } from "rollup-plugin-terser";
 
