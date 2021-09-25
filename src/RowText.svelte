dba69cb46302c6ad636990f3dcd8220abf85f419
Swiftaff <swiftaff@gmail.com>
2021-09-25 23:14:45 +0800

    RowText - better formatted

--- src/RowText.svelte
+++ src/RowText.svelte
@@ -5,27 +5,47 @@
 
 {#if row}
     {#if row.html}
-        {#if isExpanded}{#if row.expandable}{@html row.expandable}{:else}{@html row.html}{/if}{:else}{@html row.html}{/if}
+        {#if isExpanded}
+            {#if row.expandable}
+                {@html row.expandable}
+            {:else}
+                {@html row.html}
+            {/if}
+        {:else}
+            {@html row.html}
+        {/if}
     {:else}
-        <!-- if typeof row.val !== "undefined"} -->
-        <span
-            >{#if isExpanded}{" ".repeat(row.indent)}{#if "key" in row && row.key !== ""}<span class="key"
-                        >{row.key}</span
-                    >:
-                {/if}<span class="val"
-                    >{#if row.type}{row.val.substring(0, row.val.length - row.bracket)}{:else}{row.val}{/if}</span
-                >{:else}{" ".repeat(row.indent)}{#if "key" in row && row.key !== ""}<span class="key">{row.key}</span>:
+        <span>
+            {#if isExpanded}
+                {" ".repeat(row.indent)}
+                {#if "key" in row && row.key !== ""}
+                    <span class="key">{row.key}:</span>
+                {/if}<span class="val">
+                    {#if row.type}
+                        {row.val.substring(0, row.val.length - row.bracket)}
+                    {:else}
+                        {row.val}
+                    {/if}
+                </span>
+            {:else}
+                {" ".repeat(row.indent)}
+                {#if "key" in row && row.key !== ""}
+                    <span class="key">{row.key}: </span>
                 {/if}
-                {#if (!row.is_multiline || (row.is_multiline && row.is_first_multiline)) && (row.type === "string" || row.format_type === "string")}<span
-                        class="white">"</span
-                    >{/if}<span class="val">{row.val}</span
-                >{#if (!row.is_multiline || (row.is_multiline && row.is_last_multiline)) && (row.type === "string" || row.format_type === "string")}<span
-                        class="white">"</span
-                    >{/if}{/if}
-            {#if row.type && row.type !== "ARRAY+OBJECT" && row.type !== "ARRAY+SUB_ARRAY"}<span class="type"
-                    >{row.type}</span
-                >{/if}
-            {#if row.len}<span class={"len" + (isExpanded ? " grey" : "")}>({row.len})</span>{/if}
+                {#if (!row.is_multiline || (row.is_multiline && row.is_first_multiline)) && (row.type === "string" || row.format_type === "string")}
+                    <span class="white">"</span>
+                {/if}
+                <span class="val">{row.val}</span>
+                {#if (!row.is_multiline || (row.is_multiline && row.is_last_multiline)) && (row.type === "string" || row.format_type === "string")}
+                    <span class="white">"</span>
+                {/if}
+            {/if}
+            {#if row.type && row.type !== "ARRAY+OBJECT" && row.type !== "ARRAY+SUB_ARRAY"}
+                <span class="type">{row.type}</span>
+            {/if}
+            {#if row.len}
+                <span class={"len" + (isExpanded ? " grey" : "")}>({row.len})</span>
+            {/if}
         </span>
     {/if}
 {/if}
