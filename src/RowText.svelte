456cb7817d4fe5e0572568409d016a908fd51274
Swiftaff <swiftaff@gmail.com>
2021-09-25 23:02:30 +0800

    Fix missing close tags

--- src/RowText.svelte
+++ src/RowText.svelte
@@ -12,21 +12,20 @@
             >{#if isExpanded}{" ".repeat(row.indent)}{#if "key" in row && row.key !== ""}<span class="key"
                         >{row.key}</span
                     >:
+                {/if}<span class="val"
+                    >{#if row.type}{row.val.substring(0, row.val.length - row.bracket)}{:else}{row.val}{/if}</span
+                >{:else}{" ".repeat(row.indent)}{#if "key" in row && row.key !== ""}<span class="key">{row.key}</span>:
                 {/if}
-                <span class="val">{row.val.substring(0, row.val.length - row.bracket)}</span>
-            {:else}{" ".repeat(row.indent)}{#if "key" in row && row.key !== ""}<span class="key">{row.key}</span>: {/if}
                 {#if (!row.is_multiline || (row.is_multiline && row.is_first_multiline)) && (row.type === "string" || row.format_type === "string")}<span
                         class="white">"</span
                     >{/if}<span class="val">{row.val}</span
                 >{#if (!row.is_multiline || (row.is_multiline && row.is_last_multiline)) && (row.type === "string" || row.format_type === "string")}<span
                         class="white">"</span
                     >{/if}{/if}
-            {#if row.type && row.type !== "ARRAY+OBJECT" && row.type !== "ARRAY+SUB_ARRAY"}
-                <span class="type">{row.type}</span>
-            {/if}
-            {#if row.len}
-                <span class={"len" + (isExpanded ? " grey" : "")}>({row.len})</span>
-            {/if}
+            {#if row.type && row.type !== "ARRAY+OBJECT" && row.type !== "ARRAY+SUB_ARRAY"}<span class="type"
+                    >{row.type}</span
+                >{/if}
+            {#if row.len}<span class={"len" + (isExpanded ? " grey" : "")}>({row.len})</span>{/if}
         </span>
     {/if}
 {/if}
