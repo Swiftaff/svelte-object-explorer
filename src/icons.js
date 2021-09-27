2532f09434cb45b668d9fdd952364b1df325840f
Swiftaff <swiftaff@gmail.com>
2021-09-27 21:16:25 +0800

    commit icons.js - needed for default Svelte dist

---
+++ src/icons.js
@@ -0,0 +1,46 @@
+/*
+This file was generated directly by 'https://github.com/Swiftaff/svelte-iconify-svg' 
+or via the rollup plugin 'https://github.com/Swiftaff/rollup-plugin-iconify-svg'.
+*/
+
+export default {
+  "fluent:chevron-down-12-filled": `<svg
+width="100%" 
+xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+aria-hidden="true"
+focusable="false"
+style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg);
+transform: rotate(360deg);"
+preserveAspectRatio="xMidYMid meet"
+viewBox="0 0 12 12">
+<g fill="none"><path d="M2.22 4.47a.75.75 0 0 1 1.06 0L6 7.19l2.72-2.72a.75.75 0 0 1 1.06 1.06L6.53 8.78a.75.75 0 0 1-1.06 0L2.22 5.53a.75.75 0 0 1 0-1.06z" fill="currentColor"/></g>
+<rect x="0" y="0" width="12" height="12" fill="rgba(0, 0, 0, 0)" />
+</svg>`,
+    "fluent:chevron-right-12-filled": `<svg
+width="100%" 
+xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+aria-hidden="true"
+focusable="false"
+style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg);
+transform: rotate(360deg);"
+preserveAspectRatio="xMidYMid meet"
+viewBox="0 0 12 12">
+<g fill="none"><path d="M4.47 2.22a.75.75 0 0 0 0 1.06L7.19 6L4.47 8.72a.75.75 0 0 0 1.06 1.06l3.25-3.25a.75.75 0 0 0 0-1.06L5.53 2.22a.75.75 0 0 0-1.06 0z" fill="currentColor"/></g>
+<rect x="0" y="0" width="12" height="12" fill="rgba(0, 0, 0, 0)" />
+</svg>`,
+    "fluent:chevron-up-12-filled": `<svg
+width="100%" 
+xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+aria-hidden="true"
+focusable="false"
+style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg);
+transform: rotate(360deg);"
+preserveAspectRatio="xMidYMid meet"
+viewBox="0 0 12 12">
+<g fill="none"><path d="M2.22 7.53a.75.75 0 0 0 1.06 0L6 4.81l2.72 2.72a.75.75 0 0 0 1.06-1.06L6.53 3.22a.75.75 0 0 0-1.06 0L2.22 6.47a.75.75 0 0 0 0 1.06z" fill="currentColor"/></g>
+<rect x="0" y="0" width="12" height="12" fill="rgba(0, 0, 0, 0)" />
+</svg>`,
+    }
