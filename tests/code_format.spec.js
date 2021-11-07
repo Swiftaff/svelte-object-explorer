f1bf8a6d4c1f25d6260c6374db4f3142bdbd3a5b
Swiftaff <swiftaff@gmail.com>
2021-11-07 16:03:36 +0800

    terser test

--- tests/code_format.spec.js
+++ tests/code_format.spec.js
@@ -1,7 +1,21 @@
+const fs = require("fs");
 const test = require("ava");
 const options = { array_length_max: 3 };
 
 import { recursive_get_chunked_array } from "../src/lib/codeFormat.js";
+
+test("Bundles use terser", async (t) => {
+    const dist_index_actual = fs.readFileSync("./dist/index.js", "utf8").substring(0, 52);
+    const dist_index_mjs_actual = fs.readFileSync("./dist/index.mjs", "utf8").substring(0, 39);
+    const dist_value_mjs_actual = fs.readFileSync("./dist/value.mjs", "utf8").substring(0, 39);
+
+    const dist_index_expected = "var SvelteObjectExplorerCustomElementIIFE=function()";
+    const dist_index_and_value_mjs_expected = "function t(){}function e(t){return t()}";
+
+    t.deepEqual(dist_index_actual, dist_index_expected);
+    t.deepEqual(dist_index_mjs_actual, dist_index_and_value_mjs_expected);
+    t.deepEqual(dist_value_mjs_actual, dist_index_and_value_mjs_expected);
+});
 
 test("recursive_get_chunked_array 0 for 3s", async (t) => {
     const input = [];
